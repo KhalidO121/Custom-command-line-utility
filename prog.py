4315f9a0cdb990b804b1b06b7050728b47016f25
KhalidO121 <khalidokewole383@gmail.com>
2024-10-01 21:04:26 +0100

    Setup of parser and basic processing of file command line arguments.

--- prog.py
+++ prog.py
@@ -1,8 +1,15 @@
 import argparse
 
+# This Block sets up command line tool
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", help="number of bytes in a file", action="store_true")
+parser.add_argument(
+    "file", help="file to be processed", type=argparse.FileType("r", encoding="latin-1")
+)
 
-parser = argparse.ArgumentParser(description="Process some integers.")
-
-
+# This Block handles file positional argument
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+file = args.file
+with open(file.name, "r") as f:
+    first_line = f.readline()
+    print(first_line)
