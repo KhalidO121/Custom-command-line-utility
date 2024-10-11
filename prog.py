4d2d3478e6a3763c953c1b4041e3b8084c2bf4f4
KhalidO121 <khalidokewole383@gmail.com>
2024-10-12 00:21:00 +0100

    Function for counting number of bytes in a file

--- prog.py
+++ prog.py
@@ -1,15 +1,23 @@
 import argparse
 
-# This Block sets up command line tool
+
+# Count the number of bytes in a file
+def number_of_bytes(file):
+    total_bytes = 0
+    file_lines_list = file.readlines()
+    for line in file_lines_list:
+        total_bytes += len(line.encode("utf-8"))
+    print(total_bytes)
+
+
+# Sets up command line tool and arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", help="number of bytes in a file", action="store_true")
 parser.add_argument(
-    "file", help="file to be processed", type=argparse.FileType("r", encoding="latin-1")
+    "file", help="file to be processed", type=argparse.FileType("r", encoding="utf-8")
 )
 
-# This Block handles file positional argument
+# Capture Command line positional
 args = parser.parse_args()
-file = args.file
-with open(file.name, "r") as f:
-    first_line = f.readline()
-    print(first_line)
+if args.c and args.file:
+    result = number_of_bytes(args.file)
