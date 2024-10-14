1b3a7c1fb8fed38fceef8f781ab45eddb882615a
KhalidO121 <khalidokewole383@gmail.com>
2024-10-14 13:41:50 +0100

    Update

--- prog.py
+++ prog.py
@@ -1,7 +1,6 @@
 import argparse
 
 
-# Count the number of bytes in a file
 def number_of_bytes(file):
     total_bytes = 0
     file_lines_list = file.readlines()
@@ -10,14 +9,12 @@ def number_of_bytes(file):
     print(total_bytes)
 
 
-# Sets up command line tool and arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", help="number of bytes in a file", action="store_true")
 parser.add_argument(
     "file", help="file to be processed", type=argparse.FileType("r", encoding="utf-8")
 )
 
-# Capture Command line positional
 args = parser.parse_args()
 if args.c and args.file:
-    result = number_of_bytes(args.file)
+    number_of_bytes(args.file)
