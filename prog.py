a0e7b7449364c9b195ec67d720da5ad30826faa1
KhalidO121 <khalidokewole383@gmail.com>
2024-10-23 22:46:06 +0100

    Added the function that finds number of characters in a file

--- prog.py
+++ prog.py
@@ -3,13 +3,13 @@ import os
 
 
 def number_of_bytes(file):
-    print(f"  {os.path.getsize(file.name)} {file.name}")
+    return os.path.getsize(file.name)
 
 
 def number_of_lines(file):
     fh = open(file.name, "r")
     file_lines = fh.readlines()
-    print(f"  {len(file_lines)} {file.name}")
+    return len(file_lines)
 
 
 def number_of_words(file):
@@ -18,21 +18,33 @@ def number_of_words(file):
     file_lines = fh.readlines()
     for line in file_lines:
         total_words += len(line.split())
-    print(f"  {total_words} {file.name}")
+    return total_words
+
+
+def number_of_characters(file):
+    total_characters = 0
+    fh = open(file.name, "r")
+    file_lines = fh.readlines()
+    for line in file_lines:
+        total_characters += len(line.replace("\n", "  "))
+    return total_characters
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", help="number of bytes in a file", action="store_true")
 parser.add_argument("-l", help="number of lines in a file", action="store_true")
 parser.add_argument("-w", help="number of words in a file", action="store_true")
+parser.add_argument("-m", help="number of characters in a file", action="store_true")
 parser.add_argument(
     "file", help="file to be processed", type=argparse.FileType("r", encoding="utf-8")
 )
 
 args = parser.parse_args()
 if args.c and args.file:
-    number_of_bytes(args.file)
+    print(f"  {number_of_bytes(args.file)} {args.file.name}")
 elif args.l and args.file:
-    number_of_lines(args.file)
+    print(f" {number_of_lines(args.file)} {args.file.name}")
 elif args.w and args.file:
-    number_of_words(args.file)
+    print(f"  {number_of_words(args.file)} {args.file.name}")
+elif args.m and args.file:
+    print(f"  {number_of_characters(args.file)} {args.file.name}")
